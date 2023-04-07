8a21eca45c1b260617bf870a6208aa428b250429
Benjamim-EP <bannk18@gmail.com>
2023-04-07 18:45:07 -0300

    de novo?

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const http = require('http');
-const app = require('../app');
+//const app = require('../app');
+{
+    // parte do app
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+}
+
 const port = process.env.PORT || 3000; // caso a porta n esteja definida, usar a porta 3000
 const server = http.createServer(app);
 server.listen(port);
